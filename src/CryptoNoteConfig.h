8f816146a225114bb0ddb7f8a82dfab98212cdbb
p2svn <33637233+p2svn@users.noreply.github.com>
2017-12-18 14:41:18 +0700

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -119,7 +119,7 @@ const uint64_t P2P_DEFAULT_INVOKE_TIMEOUT                    = 60 * 2 * 1000; //
 const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          // 5 seconds
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "";
 
-const char* const SEED_NODES[] = { "127.0.0.1:4444" };
+const char* const SEED_NODES[] = { "61.14.237.208:44444","104.156.230.224:44444" };
 
 
 struct CheckpointData {
